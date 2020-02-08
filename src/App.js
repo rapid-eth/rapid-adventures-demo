e906cb584502e446fb518ab35469dfdb2ab679dc
Pat <me@onpaws.com>
2020-02-08 02:17:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import './App.css'
+
+import start from './images/start.png'
+//onboarding
+import localhost_3000_joinus from './images/localhost_3000_joinus.png';
+import localhost_3000_joinus1 from './images/localhost_3000_joinus (1).png';
+import localhost_3000_joinus3 from './images/localhost_3000_joinus (3).png';
+import localhost_3000_joinus8 from './images/localhost_3000_joinus (8).png';
+
+//dashboard
+import dashboard from './images/localhost_3000_ (1).png';
+
+//quest
+import adventures from './images/localhost_3000_adventures.png';
+import adventures1 from './images/localhost_3000_adventures (1).png';
+import adventures3 from './images/localhost_3000_adventures (3).png';
+import adventures5 from './images/localhost_3000_adventures (5).png';
+import adventures6 from './images/localhost_3000_adventures (6).png';
+import adventures8 from './images/localhost_3000_adventures (8).png';
+import adventures9 from './images/localhost_3000_adventures (9).png';
+import adventures11 from './images/localhost_3000_adventures (11).png';
+import adventures12 from './images/localhost_3000_adventures (12).png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AwesomeSlider>
+        <div data-src={`${start}`} />
+        <div data-src={`${localhost_3000_joinus}`} />
+        <div data-src={`${localhost_3000_joinus1}`} />
+        <div data-src={`${localhost_3000_joinus3}`} />
+        <div data-src={`${localhost_3000_joinus8}`} />
+        <div data-src={`${dashboard}`} />
+        <div data-src={`${adventures}`} />
+        <div data-src={`${adventures1}`} />
+        <div data-src={`${adventures3}`} />
+        <div data-src={`${adventures5}`} />
+        <div data-src={`${adventures6}`} />
+        <div data-src={`${adventures8}`} />
+        <div data-src={`${adventures9}`} />
+        <div data-src={`${adventures11}`} />
+        <div data-src={`${adventures12}`} />
+      </AwesomeSlider>
     </div>
   );
 }
